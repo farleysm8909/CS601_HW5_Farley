e6b17d4cce1bc81bc96ddf6900df3fe2b39810eb
farleysm8909 <farleysm8909@uwec.edu>
2020-12-05 13:34:42 -0600

    fixing js error

--- readme.txt
+++ readme.txt
@@ -1,4 +1,14 @@
 Week 5
 
 How Sam Farley went above and beyond
-- 
+- Added one of my personal images and image caption in addition to table when button is clicked
+- Spent considerable time styling the table with CSS, for example:
+    - color theme
+    - drop shadow
+    - alternating colors for different rows
+    - columns have equal widths
+    - sans-serif font
+- Used one of my images as the background image (faded using an external image editor)
+- Used a recursive function in script.js to populate table with JSON data
+- Commented code in script.js to improve readability/explain the logic in case it isn't clear on its own
+- Used let and template literals in script.js
--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 //Code adapted from "JSON and AJAX" example in lecture 10
 window.addEventListener("load", function(event) {
   let button = document.getElementById("button");
+  let table = document.getElementById("table");
+  let row1 = document.getElementById("row1");
+  let row2 = document.getElementById("row2");
+  let row3 = document.getElementById("row3");
+  let extraContent = document.getElementById("extra-content");
   // Create a self-invoking fxn that sets an event handler on button that calls makeRequest when clicked
   (function() {
     let httpRequest;
@@ -16,63 +21,58 @@ window.addEventListener("load", function(event) {
         return false;
       }
       // handle server response; call displayDegrees when request state changes
-      httpRequest.onreadystatechange = displayDegrees; // ready state changes after httpRequest.send() is called and response is sent back
+      httpRequest.onreadystatechange = getDegrees; // ready state changes after httpRequest.send() is called and response is sent back
       // make the request with open() and send()
       httpRequest.open('GET', url);
       httpRequest.send();
     }
     // check state of request, if done, we have a full response from server
-    function displayDegrees() {
+    function getDegrees() {
       if (httpRequest.readyState === XMLHttpRequest.DONE) {
         // check if response is 200 (worked)
         if (httpRequest.status === 200) {
           //if we wanted to access JSON content directly without parsing => document.write(httpRequest.responseText);
           //Parse JSON to turn into JS object
           let myDegrees = JSON.parse(httpRequest.responseText); //JS object with 1 key value pair, "my_degrees": [{}, {}] in the future could add .my_degrees to make this variable equal to the array instead of the object
-          let table = document.getElementById("table");
-          let row1 = document.getElementById("row1");
-          let row2 = document.getElementById("row2");
-          let row3 = document.getElementById("row3");
-          let extraContent = document.getElementById("extra-content");
+          //create counter variable and call function to recursively display data in table
           let count = 0;
-          let count2 = 0;
-          /* Code adapted from https://www.tutorialrepublic.com/javascript-tutorial/javascript-json-parsing.php
-          * iterates through array of degree objects and examines their key-value pairs, 
-          * then recursively calls this function if the value is an object to get to the key-value pairs nested inside that object 
-          * Chose this approach as opposed to jQuery, was curious how it could be done without it */
-          function printDegrees(myDegrees) {
-            for (let degree in myDegrees) { 
-              if (myDegrees[degree] instanceof Object) { //if the value is an object, recursively call function to access object's properties
-                if (count === 2) { //if we have cycled past the outer objects, we are now looking at the value of the first "degree" object, which is itself an object
-                  let keys = Object.keys(myDegrees[degree]); //array of the object's keys: ["school", "program/major", "type", "year conferred"]
-                  for (let i = 0; i < keys.length; i++) {
-                    row1.innerHTML += `<th>${keys[i]}</th>`; //print each of these keys out and add to the first row of the table (headings)
-                  }
-                }
-                count++; //Keep track of which object we're on
-                printDegrees(myDegrees[degree]); //recursively call to examine more objects' key-value pairs
-              } else {
-                if (count2 < 4) { //has to cycle past 4 table data for second row before proceeding to else
-                  row2.innerHTML += `<td>${myDegrees[degree]}</td>`;
-                } else {
-                  row3.innerHTML += `<td>${myDegrees[degree]}</td>`;
-                }
-                count2++;
-              }
-            }
-            table.style.visibility = "visible"; //make table visible after it is populated
-            extraContent.innerHTML = `   
-              <img src="IMG_0395.JPG" alt="Sam Farley with her parents at graduation" width="500">
-              <p>Sam Farley celebrating her virtual graduation during the onset of the pandemic in May, 2020</p>
-            `;
-            extraContent.style.visibility = "visible";
-          }
-          printDegrees(myDegrees);
+          printDegrees(myDegrees, count);
         } else {
           // AJAX call unsuccessful
           alert("Error! There was a problem with the request.");
         }
       }
+    }
+    /* Code adapted from https://www.tutorialrepublic.com/javascript-tutorial/javascript-json-parsing.php
+    * iterates through array of degree objects and examines their key-value pairs, 
+    * then recursively calls this function if the value is an object to get to the key-value pairs nested inside that object 
+    * Chose this approach as opposed to jQuery, was curious how it could be done without it */
+    function printDegrees(myDegrees, count) {
+      for (let degree in myDegrees) { 
+        if (myDegrees[degree] instanceof Object) { //if the value is an object, recursively call function to access object's properties
+          if (count === 3) { //if we have cycled past the outer objects, we are now looking at the value of the first "degree" object, which is itself an object
+            let keys = Object.keys(myDegrees[degree]); //array of the object's keys: ["school", "program/major", "type", "year conferred"]
+            for (let i = 0; i < keys.length; i++) {
+              row1.innerHTML += `<th>${keys[i]}</th>`; //print each of these keys out and add to the first row of the table (headings)
+            }
+          }
+          count++; //Keep track of which object we're on
+          printDegrees(myDegrees[degree], count, count2); //recursively call to examine more objects' key-value pairs
+        } else {
+          if (count < 4) { //has to cycle past table data for second row before proceeding to else
+            row2.innerHTML += `<td>${myDegrees[degree]}</td>`;
+          } else {
+            row3.innerHTML += `<td>${myDegrees[degree]}</td>`;
+          }
+          //alert("count1: " + count + "\ncount2: " + count2 + "\nrow2.innerHTML = " + row2.innerHTML + "\nrow3.innerHTML = " + row3.innerHTML);
+        }
+      }
+      table.style.visibility = "visible"; //make table visible after it is populated
+      extraContent.innerHTML = `   
+        <img src="IMG_0395.JPG" alt="Sam Farley with her parents at graduation" width="500">
+        <p>Sam Farley celebrating her virtual graduation during the onset of the pandemic in May, 2020.</p>
+      `;
+      extraContent.style.visibility = "visible";
     }
   })();
 });
